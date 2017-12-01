52dc7a2df9cb9a0714be66f4dd2c71c28f0461ec
ZhiYajing <chinesezhiyajing@163.com>
2017-12-01 09:51:10 +0800

    Add files via upload

---
+++ editdistance/mainEditDistance.java
@@ -0,0 +1,9 @@
+
+
+public class mainEditDistance
+{
+    public static void main(String[] args){
+        editdistance D1= new editdistance();
+        D1.getDistance("hbvhjbvjhb","oj;jjihj");
+    }
+}

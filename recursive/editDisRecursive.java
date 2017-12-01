877a58bf3c7bc27a27a9f391eb6b6fdec7ccefe2
ZhiYajing <chinesezhiyajing@163.com>
2017-12-01 11:57:58 +0800

    Update editDisRecursive.java

--- recursive/editDisRecursive.java
+++ recursive/editDisRecursive.java
@@ -1,60 +1,44 @@
-
-public class editDisRecursive
+class EDIST
 {
-    public editDisRecursive()
+    static int min(int x,int y,int z)
     {
-
+        if (x<=y && x<=z) return x;
+        if (y<=x && y<=z) return y;
+        else return z;
     }
-    private int distance =-1;
-    public  int getDistance(String strA, String strB,int n,int m)
+ 
+    static int editDist(String str1 , String str2 , int m ,int n)
     {
-        int lengthA=strA.length();
-        int lengthB=strB.length();
-        int a[] =new int[lengthA+1];
-        int b[] =new int[lengthB+1];
-        
-        
-        for (int i=0;i<strA.length();i++){
-            a[i]=strA.charAt(i);
-        }
-        for (int j=0;j<strB.length();j++){
-            b[j]=strB.charAt(j);
-        }
-
-        if(null==strA || null ==strB || strA.isEmpty()||strB.isEmpty())
-        {
-            distance=-1;
-        }
-        else if(strA.equals(strB))
-        {
-            distance =0;
-            
-        }
-        else if (m==0)
-        {
-            distance =n;
-        }else if (n==0)
-        {
-            distance =m;
-        }
-        else {
-            for(int i=1;i<=n;i++)
-            {
-                for(int j=1;j<=m;j++)
-                {
-                    if(a[i-1]==b[j-1])
-                    {
-                        distance =getDistance( strA,  strB ,n-1,m-1);
-                    }else{
-                        distance= Math.min(getDistance( strA,  strB ,n-1,m)+1,
-                            Math.min(getDistance( strA,  strB ,n,m-1)+1,getDistance( strA,  strB ,n-1,m-1)+1));
-                    }
-                }
-            }
-
-        }
-        System.out.println(distance);
-        return distance;
+        // If first string is empty, the only option is to
+    // insert all characters of second string into first
+    if (m == 0) return n;
+      
+    // If second string is empty, the only option is to
+    // remove all characters of first string
+    if (n == 0) return m;
+      
+    // If last characters of two strings are same, nothing
+    // much to do. Ignore last characters and get count for
+    // remaining strings.
+    if (str1.charAt(m-1) == str2.charAt(n-1))
+        return editDist(str1, str2, m-1, n-1);
+      
+    // If last characters are not same, consider all three
+    // operations on last character of first string, recursively
+    // compute minimum cost for all three operations and take
+    // minimum of three values.
+    return 1 + min ( editDist(str1,  str2, m, n-1),    // Insert
+                     editDist(str1,  str2, m-1, n),   // Remove
+                     editDist(str1,  str2, m-1, n-1) // Replace                     
+                   );
     }
-
+ 
+    public static void main(String args[])
+    {
+        String str1 = "sunday";
+        String str2 = "saturday";
+  
+        System.out.println( editDist( str1 , str2 , str1.length(), str2.length()) );
+    }
 }
+/*This code is contributed by Rajat Mishra*/
